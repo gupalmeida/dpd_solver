Fixed a bug in IOtools.exportFields for getting the correct number of cell in each direction. Added numerics.py which will contain the numerical methods for solving either Eulerian or Lagrangian problems. Broke mesh input files from input.inp so that user can specify more parameters for preProcess.mesh such as creating or reading a mesh, however, reading mesh and creating unstructured mesh are not yet implemented.

src/IOtools.py
@@ -2,10 +2,10 @@ import os
 import sys
 import numpy as np
 
-def getValue(varname,dtype='int'):
-    """ This method gets value of a given variable from input file. Variable name is given as string and the method returns numerical value according to dtype argument given by user. dtype argument must be passed as string and its default value is 'int'."""
+def getValue(varname,filename='input.inp',dtype='int'):
+    """ Gets value of a given variable from input file. Variable name is given as string and the method returns numerical value according to dtype argument given by user. dtype argument must be passed as string and its default value is 'int'."""
     # defining input file path and file name
-    fp = os.getcwd() + "/input.inp"
+    fp = os.getcwd() + '/' + filename
     f = open(fp,'r')
     value = ""
 
@@ -27,10 +27,12 @@ def getValue(varname,dtype='int'):
     return eval(cmd)
 
 def exportFields(cname,solution=None,timeStep=None,varList=list()):
-    """ This method writes output file in dat format for visualizing data in ParaView or Tecplot. For temporal analysis the parameter 'timeStep' should be provided. Also, a list of variable names may be provided in order to allow the method to write their respective values."""
+    """ Writes output file in dat format for visualizing data in ParaView or Tecplot. For temporal analysis the parameter 'timeStep' should be provided. Also, a list of variable names may be provided in order to allow the method to write their respective values."""
 
     if solution is not None:
         # GETTING COORDINATE VECTORS FROM SOLUTION
+        # solution tuple is given in the following order
+        # (n_dim, x_coord, y_coord, z_coord, solution_variables)
         x = solution[0]
         y = solution[1]
         z = solution[2]
@@ -56,8 +58,11 @@ def exportFields(cname,solution=None,timeStep=None,varList=list()):
         # WRITING SOLUTION
         # mapping variables into strings for writing
         data = np.array([arr.astype(str) for arr in solution])
-        for j in range(len(y)):
-            for i in range(len(x)):
+
+        # getting number of cells in each direction
+        _i,_j,_k = np.shape(solution[0])
+        for j in range(_j):
+            for i in range(_i):
                 x_ = data[0]
                 y_ = data[1]
                 z_ = data[2]

src/preProcess.py
@@ -5,7 +5,16 @@ import IOtools as io
 class mesh:
     """ The mesh class presents methods for importing, exporting and generating computational meshes for later use in simulations. """
 
-    def genStrcdBlock(self,origin=(0,0,0),dim_x=1,dim_y=1,dim_z=1,
+    def __init__(self):
+        meshfile = 'mesh.inp'
+        self.action = io.getValue('meshAction',filename=meshfile,dtype='str')
+        self.mshtype = io.getValue('meshType',filename=meshfile,dtype='str')
+        self.writeMesh = io.getValue('writeMesh',filename=meshfile,dtype='bool')
+        self.nx = io.getValue('nx',filename=meshfile,dtype='int')
+        self.ny = io.getValue('ny',filename=meshfile,dtype='int')
+        self.nz = io.getValue('nz',filename=meshfile,dtype='int')
+
+    def __genStructBlock(self,origin=(0,0,0),dim_x=1,dim_y=1,dim_z=1,
             nx=10,ny=10,nz=1,indexing='ij',write_msh_file=False):
         """ Generates a simple structured block mesh. The user should pass the origin of the block as a tuple, the dimensions in x, y and z directions (dim_x, dim_y and dim_z), as well as the number of nodes in each direction (nx, ny and nz). """
 
@@ -27,5 +36,19 @@ class mesh:
 
         # obtaining block mesh coordinate arrays
         x,y,z = np.meshgrid(x_,y_,z_,indexing=indexing)
+        data = (x,y,z)
 
-        return (x,y,z)
+        # writing mesh file
+        if write_msh_file:
+            io.exportFields("mesh",solution=data)
+
+        return data
+
+    def createMesh(self):
+        if self.action == 'create' and self.mshtype == 'structured':
+            return self.__genStructBlock(nx=self.nx,ny=self.ny,nz=self.nz,write_msh_file=self.writeMesh)
+        elif self.action == 'create' and self.mshtype == 'unstructured':
+            print("Unstructured mesh not implemented yet")
+        elif self.action == 'read':
+            print("Read mesh not implemented yet")
+